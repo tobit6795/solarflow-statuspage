67b5a368108adab48705c897af476c07774ba335
kekem <kem.kevin2002@gmail.com>
2023-09-13 19:59:19 +0200

    updated build.sh, edit gitignore, added online-mode to dockerfile

--- src/build.sh
+++ src/build.sh
@@ -7,4 +7,4 @@ docker stop ${IMAGE} || echo cannot stop, ${IMAGE} does not exist
 docker rm ${IMAGE} || echo Cannot delete, ${IMAGE} does not exist
 docker build -t ${USER}/${IMAGE}:$tag .
 docker image push ${USER}/${IMAGE}:$tag
-docker run -d --restart=always --env-file .env --name ${IMAGE} tobit6795/${IMAGE}
+docker run -d --restart=always -p 127.0.0.1:5000:5000 --env-file .env --name ${IMAGE} ${USER}/${IMAGE}
